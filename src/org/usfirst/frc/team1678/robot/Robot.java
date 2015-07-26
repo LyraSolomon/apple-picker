18fc6824d94f71c730cf6c5566078a0cde41827b
1678 Programming <1678programming@gmail.com>
2015-07-26 11:47:00 -0700

    added elevator code

--- src/org/usfirst/frc/team1678/robot/Robot.java
+++ src/org/usfirst/frc/team1678/robot/Robot.java
@@ -2,42 +2,32 @@
 package org.usfirst.frc.team1678.robot;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.*;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-/**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the IterativeRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the manifest file in the resource
- * directory.
- */
 public class Robot extends IterativeRobot {
-    /**
-     * This function is run when the robot is first started up and should be
-     * used for any initialization code.
-     */
+    YElevator y;
+    
     public void robotInit() {
-
+    	y=new YElevator();
+		SmartDashboard.putBoolean("is terminated2", false);
     }
 
-    /**
-     * This function is called periodically during autonomous
-     */
-    public void autonomousPeriodic() {
-
+    public void autonomousInit(){}
+    public void autonomousPeriodic() {}
+    
+    public void teleopInit() {
+    	y.runCalibration();
+    	y.setTargetHeight(1);
     }
-
-    /**
-     * This function is called periodically during operator control
-     */
     public void teleopPeriodic() {
-        
+    	y.periodic();
     }
     
-    /**
-     * This function is called periodically during test mode
-     */
-    public void testPeriodic() {
+    public void testInit(){}
+    public void testPeriodic() {}
     
+    public void disabledInit(){}
+    public void disabledPeriodic(){
     }
-    
 }
